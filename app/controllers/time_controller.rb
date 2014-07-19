1860a8a993e3c380290848b85c35ce15c7c88104
Raj Solanki <rajmahaj@gmail.com>
2014-07-18 20:10:48 -0600

    Edits

--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -1,4 +1,8 @@
 class TimeController < ApplicationController
   def now
+    # Time.now returns the current time
+    # Time.now.utc returns the current time in UTC
+    # strftime uses format strings to format the output of the time
+    @localtime = Time.now.strftime('%a, %d %b %Y %H:%M:%S UTC %z')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:name' => 'welcome#hello'
+
+  # When I receive a GET request to
+  # the /time/now URL, send it to
+  # the time controller, now method (action)
   get 'time/:now' => 'time#now'
   get 'introduce/:name1/and/:name2' => 'introduce#people'
 end
